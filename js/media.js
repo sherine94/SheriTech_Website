7e9dabbe2a5e01f1a2765e0d3def085a3c6f62ff
sherine94 <61482091+sherine94@users.noreply.github.com>
2020-08-26 20:04:02 +0100

    Update Js
    DOM added to the code

--- js/media.js
+++ js/media.js
@@ -1,1 +1,13 @@
 
+/*global document*/
+/*global alert*/
+/*eslint-env es6*/
+
+const clicked = document.getElementById("form");
+
+clicked.addEventListener("submit",something);
+
+function something ()
+{
+    alert ("the form has been submitted successfully");
+}
